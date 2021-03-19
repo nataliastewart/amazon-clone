aa8d7a46a16d343f695792cf5fcab7fe7eb6b654
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-19 16:34:21 -0400

    Added ShoppingBasket into the nav bar - styled

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
 	return (
@@ -13,18 +14,24 @@ function Header() {
 				<input className="header__searchInput" type="text" />
 				<SearchIcon className="header__searchIcon" />
 			</div>
-			<div className="header__nav"></div>
-			<div className="header__option">
-				<span className="header__optionLineOne">Hello Guest</span>
-				<span className="header__optionLineTwo">Sign In</span>
+			<div className="header__nav">
+				<div className="header__option">
+					<span className="header__optionLineOne">Hello Guest</span>
+					<span className="header__optionLineTwo">Sign In</span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Returns</span>
+					<span className="header__optionLineTwo">& Orders </span>
+				</div>
+				<div className="header__option">
+					<span className="header__optionLineOne">Your</span>
+					<span className="header__optionLineTwo">Prime</span>
+				</div>
 			</div>
-			<div className="header__option">
-				<span className="header__optionLineOne">Returns</span>
-				<span className="header__optionLineTwo">& Orders </span>
-			</div>
-			<div className="header__option">
-				<span className="header__optionLineOne">Your</span>
-				<span className="header__optionLineTwo">Prime</span>
+
+			<div className="header__optionBasket">
+				<ShoppingBasketIcon />
+				<span className="header__optionLineTwo header__optionBasket">0</span>
 			</div>
 		</div>
 	);
