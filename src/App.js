356e8aefa7806db4af752ff45792f6cabed64c20
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-20 11:45:09 -0400

    Creates Home.js - Added Hero image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './Header.js';
+import Home from './Home';
 
 function App() {
 	return (
 		//BEM convention for styling - lowercase "app"
 		<div className="app">
 			<Header />
-			{/*Home */}
+			<Home/>
 		</div>
 	);
 }
