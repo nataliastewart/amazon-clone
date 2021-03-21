4cac8a5e88758d080219646fbec06e01ec8af073
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-20 20:56:36 -0400

    Creates Product.js - Product.css

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Product.css';
+
+export default function Product() {
+	return (
+		<div className="product">
+			<div className="product__info">
+				<p>
+					The Lean Startup: How Constant Innovation Creates Radically Successful
+					Businesses Paperback
+				</p>
+				<p className="product__price">
+					<small>$</small>
+					<strong>19.99</strong>
+				</p>
+				<div className="product__rating">
+					<p>⭐</p>
+					<p>⭐</p>
+					<p>⭐</p>
+				</div>
+			</div>
+		</div>
+	);
+}
