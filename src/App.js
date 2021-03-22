85b92f7c646bc3a5238ed218e7fe7bc9275ce75a
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-21 20:18:10 -0400

    Added Link to Amazon logo and ShoppingBasket - Header.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Header from './Header.js';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
 	return (
 		//BEM convention for styling - lowercase "app"
+		//Important: The Header must be outside of the switch
 		<Router>
 			<div className="app">
+				<Header />
 				<Switch>
+					<Route path="/checkout">
+						<Checkout />
+					</Route>
 					<Route path="/">
-						<Header />
 						<Home />
 					</Route>
 				</Switch>
