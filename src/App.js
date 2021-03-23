61615d788c8e785ff2a3c13fa32415a7f20aacae
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-23 15:22:45 -0400

    Login.js styled Login.css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './Header.js';
 import Home from './Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Checkout from './Checkout';
+import Login from './Login';
 
 function App() {
 	return (
@@ -10,12 +11,16 @@ function App() {
 		//Important: The Header must be outside of the switch
 		<Router>
 			<div className="app">
-				<Header />
 				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
 					<Route path="/checkout">
+						<Header />
 						<Checkout />
 					</Route>
 					<Route path="/">
+						<Header />
 						<Home />
 					</Route>
 				</Switch>
