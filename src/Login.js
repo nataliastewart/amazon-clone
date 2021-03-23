a7772db91cc6187d06cfd72139ab1d013b112078
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-23 16:12:16 -0400

    Setup firebase.js - Authentication

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const signIn = e => {
+		e.preventDefault();
+		//some fancy firebase
+	};
+
+	const register = e => {
+		e.preventDefault();
+	};
+
 	return (
 		<div className="login">
 			<Link to="/">
@@ -17,17 +29,31 @@ function Login() {
 
 				<form>
 					<h5>Email</h5>
-					<input type="text" />
+					<input
+						type="text"
+						value={email}
+						onChange={e => setEmail(e.target.value)}
+					/>
 					<h5>Password</h5>
-					<input type="password" />
-					<button className="login__signInButton">Sign In</button>
+					<input
+						type="password"
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+					/>
+					<button
+						type="submit"
+						onClick={signIn}
+						className="login__signInButton"
+					>
+						Sign In
+					</button>
 				</form>
 				<p>
 					By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
 					Sale. Please see our Privacy Notice, our Cookies Notice and our
 					Interest-Based Ads Notice.
 				</p>
-				<button className="ogin__registerButton">
+				<button onClick={register} className="ogin__registerButton">
 					Create your Amazon Account
 				</button>
 			</div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,7 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from 'firebase';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyDrPtTMtLYG4fheHlUya2oOJ7uNkTcLvK4',
 	authDomain: 'clone-d3b78.firebaseapp.com',
@@ -8,3 +11,10 @@ const firebaseConfig = {
 	appId: '1:800231238777:web:b4613c774bf9a4f51978a8',
 	measurementId: 'G-P2BWQHMQ30',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
