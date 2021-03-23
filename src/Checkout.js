1d85520fedd335bd4349873612db9e2faa05fd7f
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-23 11:54:11 -0400

    Displays Product info and img into ShoppingBasket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Checkout.css';
+import CheckoutProduct from './CheckoutProduct';
+import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 export default function Checkout() {
+	const [{ basket }, dispatch] = useStateValue();
+
 	return (
 		<div className="checkout">
 			<div className="checkout__left">
@@ -11,7 +15,19 @@ export default function Checkout() {
 					src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
 					alt=""
 				/>
-				<h2 className="checkout__title">Your Shopping Basket</h2>
+				<div>
+					<h2 className="checkout__title">Your Shopping Basket</h2>
+
+					{basket.map(item => (
+						<CheckoutProduct
+							id={item.id}
+							title={item.title}
+							image={item.image}
+							price={item.price}
+							rating={item.rating}
+						/>
+					))}
+				</div>
 			</div>
 
 			<div className="checkout__right">
