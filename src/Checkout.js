592b5b569c6952899f60a836bfbdb1a726b87ed3
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-24 09:01:57 -0400

    Added user.email

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 export default function Checkout() {
-	const [{ basket }, dispatch] = useStateValue();
+	const [{ basket, user }, dispatch] = useStateValue();
 
 	return (
 		<div className="checkout">
@@ -16,6 +16,7 @@ export default function Checkout() {
 					alt=""
 				/>
 				<div>
+					<h3>Hello, {user?.email}</h3>
 					<h2 className="checkout__title">Your Shopping Basket</h2>
 
 					{basket.map(item => (
