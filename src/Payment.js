abd36c9695b41587a749ca884ebe0a3901e9cb99
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-25 08:15:22 -0400

    Setting up Payment details - Payment.js

--- src/Payment.js
+++ src/Payment.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
 import { Link } from 'react-router-dom';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format';
+import { getBasketTotal } from './reducer';
 
 function Payment() {
 	const [{ basket, user }, dispatch] = useStateValue();
+
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const [error, setError] = useState(null);
+	const [disabled, setDisabled] = useState(null);
+
+	const handleSubmit = e => {
+		//do all the facy stripe
+	};
+
+	const handleChange = event => {
+		// Listen for changes in the CardElement
+		//and display any erros as the customer types card details
+		setDisabled(event.empty);
+		setError(event.error ? event.error.message : '');
+	};
+
 	return (
 		<div className="payment">
 			<div className="payment__container">
@@ -50,7 +71,22 @@ function Payment() {
 					<div className="payment__title">
 						<h3>Payment Method</h3>
 					</div>
-					<div className="payment__details">{/*Stripe magic will go*/} </div>
+					<div className="payment__details">
+						{/*Stripe magic will go*/}
+						<form onSubmit={handleSubmit}>
+							<CardElement onChange={handleChange} />
+							<div className="payment__priceContainer">
+								<CurrencyFormat
+									renderText={value => <h3>Order Total: {value}</h3>}
+									decimalScale={2}
+									value={getBasketTotal(basket)}
+									displayType={'text'}
+									thousandSeparator={true}
+									prefix={'$'}
+								/>
+							</div>
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
