f4a72705309d2f4699de0c148867256a5ee2fcb1
Natália Miranda <nataliamirandac@hotmail.com>
2021-03-25 06:42:05 -0400

    Styled Payment.css

--- src/Payment.js
+++ src/Payment.js
@@ -2,12 +2,19 @@ import React from 'react';
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css';
 import { useStateValue } from './StateProvider';
+import { Link } from 'react-router-dom';
 
 function Payment() {
 	const [{ basket, user }, dispatch] = useStateValue();
 	return (
 		<div className="payment">
 			<div className="payment__container">
+				<div className="payment__container">
+					<h1>
+						Checkout (<Link to="checkout">{basket?.length} items)</Link>
+					</h1>
+				</div>
+
 				{/*Payment section - delivery address*/}
 				<div className="payment__section">
 					<div className="payment__title">
@@ -24,18 +31,26 @@ function Payment() {
 					<div className="payment_title">
 						<h3>Review items and delivery</h3>
 					</div>
+
+					<div className="payment__items">
+						{basket.map(item => (
+							<CheckoutProduct
+								id={item.id}
+								title={item.title}
+								image={item.image}
+								price={item.price}
+								rating={item.rating}
+							/>
+						))}
+					</div>
 				</div>
+
 				{/*Payment section - Payment method*/}
-				<div className="payment__items">
-					{basket.map(item => (
-						<CheckoutProduct
-							id={item.id}
-							title={item.title}
-							image={item.image}
-							price={item.price}
-							rating={item.rating}
-						/>
-					))}
+				<div className="payment__section">
+					<div className="payment__title">
+						<h3>Payment Method</h3>
+					</div>
+					<div className="payment__details">{/*Stripe magic will go*/} </div>
 				</div>
 			</div>
 		</div>
